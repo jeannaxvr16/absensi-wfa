8d9d4ebb59c11dd771ab384469d270aace514c0e
jeannaxvr16 <email_github_kamu@example.com>
2026-08-17 03:56:22 +0700

    Support MYSQL_URI for Railway deployment

--- config/db.js
+++ config/db.js
@@ -1,13 +1,27 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-    'absensi_wfa',
-    'root',
-    '',
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
-)
+// Menggunakan MYSQL_URI dari Railway jika ada, atau fallback ke variabel terpisah
+const db = process.env.MYSQL_URI 
+    ? new Sequelize(process.env.MYSQL_URI, {
+        dialect: 'mysql',
+        logging: false,
+        dialectOptions: {
+            connectTimeout: 60000
+        }
+      })
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+            dialect: 'mysql',
+            logging: false,
+            dialectOptions: {
+                connectTimeout: 60000
+            }
+        }
+      );
 
-module.exports = sequelize
+module.exports = db;
